eae49091d41bacf3c629c612a8e993624ca69826
Lukasz Szczygielek <lszczygielek@gmail.com>
2021-02-17 01:30:06 +0100

    Init.

---
+++ MaterialDesignInXamlToolkit-DialogHost-Focus/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MaterialDesignInXamlToolkit_DialogHost_Focus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModel viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            viewModel = new ViewModel();
+            DataContext = viewModel;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // await DialogHost.Show(string.Empty);
+            var abc = new ViewModel { LastInput = viewModel.LastInput };
+
+            var result = await DialogHost.Show(
+                //new ViewModel { LastInput = viewModel.LastInput },
+                abc,
+                (object sender, DialogClosingEventArgs args) =>
+                {
+                    if (args.Parameter is bool isConfirmed && !isConfirmed)
+                    {
+                        return;
+                        
+                    }
+                    else
+                    {
+                        viewModel.LastInput = ((ViewModel)args.Session.Content).LastInput;
+                    }
+                });
+        }
+    }
+}
---
+++ MaterialDesignInXamlToolkit-DialogHost-Focus/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MaterialDesignInXamlToolkit_DialogHost_Focus
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private string lastInput;// = "Default";
+
+        public ViewModel()
+        {
+            LastInput = "Default";
+        }
+
+        public string LastInput 
+        { 
+            get => lastInput;
+
+            set
+            {
+                lastInput = value;
+                OnPropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
